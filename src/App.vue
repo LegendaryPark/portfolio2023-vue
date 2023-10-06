1c890bfd47c59b94afd53e3890ba979a3938f688
legendaryPark <pbjkmn00@gmail.com>
2023-10-06 15:53:05 -0700

    Created components

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
+<script setup>
+import { RouterView } from 'vue-router';
+
+ </script>
+
 <template>
-  app.vue
+  <RouterView/>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,25 @@
-
+<script setup>
+  import Site from "@/components/Site.vue"
+  import Skill from "@/components/Skill.vue"
+  import Intro from "@/components/Intro.vue"
+  import Port from "@/components/Port.vue"
+  import Skip from "@/components/Skip.vue"
+  import Footer from "@/components/Footer.vue"
+  import Header from "@/components/Header.vue"
+  import Contact from "@/components/Contact.vue"
+</script>
 
 <template>
-  <main>
-    Home
+  <Skip/>
+  <Header />
+
+  <main id="main" role="main">
+    <Intro/>
+    <Skill/>
+    <Site/>
+    <Port/>
+    <Contact/>
   </main>
+  
+  <Footer/>
 </template>
